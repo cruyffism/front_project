9074715f67c1e82916585ed2d5ec92aa604664d0
tkddk0119 <tkddk0119@wisenut.co.kr>
2023-12-28 17:24:08 +0900

    주석 추가

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
-    $("#header").load("../header.html");
-    $("#footer").load("../footer.html");
+    $("#header").load("../header.html"); // id=header 인 부분에 header.html 소스 넣기
+    $("#footer").load("../footer.html"); // id=footer 인 부분에 footer.html 소스 넣기
 })
