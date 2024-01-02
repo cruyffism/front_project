4c6efaea3d4411c2e50db32d04a5aafe58c46823
minki <mkhong916@gmail.com>
2024-01-02 16:01:16 +0900

    프론트pj 수정중

--- js/common.js
+++ js/common.js
@@ -1,4 +1,7 @@
-$(document).ready(function(){
-    $("#header").load("../header.html"); // id=header 인 부분에 header.html 소스 넣기
-    $("#footer").load("../footer.html"); // id=footer 인 부분에 footer.html 소스 넣기
-})
+// $(document).ready(function(){
+//     $("#header").load("../header.html"); 
+//     $("#footer").load("../footer.html"); 
+// })
+
+// id=header 인 부분에 header.html 소스 넣기
+// id=footer 인 부분에 footer.html 소스 넣기
